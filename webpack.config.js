48bff915c2edffd544d698d41109767a0826aac7
m.kurahara <kuraharamasahiro@gmail.com>
2022-03-07 08:31:32 +0900

    editorconfigを追加

--- webpack.config.js
+++ webpack.config.js
@@ -3,74 +3,74 @@ const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/js/index.js',
-    // ファイルの出力設定
-    output: {
-        path: `${__dirname}/dist/assets/js`,
-        // 出力ファイル名
-        filename: 'script.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                '@babel/preset-env',
-                            ]
-                        }
-                    }
-                ]
+  mode: 'production',
+  entry: './src/js/index.js',
+  // ファイルの出力設定
+  output: {
+    path: `${__dirname}/dist/assets/js`,
+    // 出力ファイル名
+    filename: 'script.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
             }
+          }
         ]
-    },
-    // ES5(IE11等)向けの指定
-    target: ["web", "es5"],
-    optimization: {
-        minimizer: [
-          "...",
-          new ImageMinimizerPlugin({
-            minimizer: {
-              implementation: ImageMinimizerPlugin.imageminMinify,
-              options: {
-                plugins: [
-                  "imagemin-gifsicle",
-                  "imagemin-jpegtran",
-                  "imagemin-optipng",
-                  "imagemin-svgo",
-                ],
-              },
-            },
-            // generator: [
-            //   {
-            //     // Apply generator for copied assets
-            //     type: "asset",
-            //     implementation: ImageMinimizerPlugin.squooshGenerate,
-            //     options: {
-            //       plugins: ["imagemin-webp"],
-            //       encodeOptions: {
-            //         webp: {
-            //           quality: 100,
-            //         },
-            //       },
-            //     },
-            //   },
-            // ],
-          }),
-        ],
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                { 
-                    from: path.resolve(__dirname, 'src/images'),
-                    to: path.resolve(__dirname, 'dist/assets/images')
-                }
+      }
+    ]
+  },
+  // ES5(IE11等)向けの指定
+  target: ["web", "es5"],
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-jpegtran",
+              "imagemin-optipng",
+              "imagemin-svgo",
             ],
-        }),
+          },
+        },
+        // generator: [
+        //   {
+        //     // Apply generator for copied assets
+        //     type: "asset",
+        //     implementation: ImageMinimizerPlugin.squooshGenerate,
+        //     options: {
+        //       plugins: ["imagemin-webp"],
+        //       encodeOptions: {
+        //         webp: {
+        //           quality: 100,
+        //         },
+        //       },
+        //     },
+        //   },
+        // ],
+      }),
     ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname, 'src/images'),
+          to: path.resolve(__dirname, 'dist/assets/images')
+        }
+      ],
+    }),
+  ],
 };
